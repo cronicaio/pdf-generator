58c5a46de5c2c25a76ad98e1e3f15bb774b22416
Dmytro Kohut <retr0finr1r@gmail.com>
2019-08-06 14:55:01 +0300

    Implemented DAO for communication with Redis.

--- src/main/java/io/cronica/api/pdfgenerator/component/redis/RedisDAOImpl.java
+++ src/main/java/io/cronica/api/pdfgenerator/component/redis/RedisDAOImpl.java
@@ -1,14 +1,44 @@
 package io.cronica.api.pdfgenerator.component.redis;
 
+import io.cronica.api.pdfgenerator.component.kryo.KryoSerializer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RBinaryStream;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
 public class RedisDAOImpl implements RedisDAO {
 
+    private final RedissonClient redissonClient;
+
+    private final KryoSerializer kryoSerializer;
+
+    /**
+     * @see RedisDAO#get(String)
+     */
     @Override
     public RedisDocument get(final String key) {
+        log.info("[REDIS] reading object with '{}' key", key);
+        if ( exists(key) ) {
+            final RBinaryStream rBinaryStream = this.redissonClient.getBinaryStream(key);
+            log.info("[REDIS] found object under '{}' key", key);
+            return this.kryoSerializer.deserialize(rBinaryStream.get());
+        }
+
+        log.info("[REDIS] object with '{}' does not exists", key);
         return null;
     }
 
+    /**
+     * @see RedisDAO#exists(String)
+     */
     @Override
     public boolean exists(final String key) {
-        return false;
+        log.debug("[REDIS] check whether exists object under '{}' key", key);
+        final RBinaryStream rBinaryStream = this.redissonClient.getBinaryStream(key);
+        return rBinaryStream.isExists() && rBinaryStream.size() != 0;
     }
 }
