162412a471fba078d5186caa6f7c13aaa98e8840
Dmytro Kohut <retr0finr1r@gmail.com>
2019-08-07 11:57:59 +0300

    Generation of NonStructured document is implemented.

--- src/main/java/io/cronica/api/pdfgenerator/service/PDFDocumentServiceImpl.java
+++ src/main/java/io/cronica/api/pdfgenerator/service/PDFDocumentServiceImpl.java
@@ -1,5 +1,8 @@
 package io.cronica.api.pdfgenerator.service;
 
+import io.cronica.api.pdfgenerator.component.aws.AWSS3BucketAdapter;
+import io.cronica.api.pdfgenerator.component.aws.Repeater;
+import io.cronica.api.pdfgenerator.component.ca.CronicaCAAdapter;
 import io.cronica.api.pdfgenerator.component.entity.Document;
 import io.cronica.api.pdfgenerator.component.redis.RedisDAO;
 import io.cronica.api.pdfgenerator.component.redis.RedisDocument;
@@ -7,10 +10,14 @@ import io.cronica.api.pdfgenerator.database.model.DocumentCertificate;
 import io.cronica.api.pdfgenerator.database.repository.DocumentCertificateRepository;
 import io.cronica.api.pdfgenerator.exception.DocumentNotFoundException;
 import io.cronica.api.pdfgenerator.exception.InvalidRequestException;
+import io.cronica.api.pdfgenerator.utils.DocumentUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,7 +26,15 @@ public class PDFDocumentServiceImpl implements PDFDocumentService {
 
     private static final String PDF_DOCUMENT_TYPE = "pdf";
 
+    private final Repeater repeater;
+
     private final RedisDAO redisDAO;
+
+    private final CronicaCAAdapter cronicaCAAdapter;
+
+    private final AWSS3BucketAdapter awss3BucketAdapter;
+
+    private final DocumentTransaction documentTransaction;
 
     private final DocumentCertificateRepository documentCertificateRepository;
 
@@ -37,10 +52,14 @@ public class PDFDocumentServiceImpl implements PDFDocumentService {
             throw new DocumentNotFoundException("There is no PDF document with '" + uuid + "' UUID");
         }
 
-        final DocumentCertificate dc = getDocumentCertificateByID(redisDocument.getDocumentID());
-        final String fileName = "DC-" + redisDocument.getDocumentID() + ".pdf";
-
-        return null;
+        final DocumentCertificate docCert = getDocumentCertificateByID(redisDocument.getDocumentID());
+        if ( !docCert.getIsStructured() ) {
+            return downloadNonStructuredDocument(redisDocument);
+        }
+        else {
+            // TODO: implement for StructuredDocument
+            return null;
+        }
     }
 
     private void validateUUID(final String uuid) {
@@ -67,4 +86,30 @@ public class PDFDocumentServiceImpl implements PDFDocumentService {
         log.info("[DATABASE] document with '{}' ID has been found", dc.toString());
         return dc;
     }
+
+    private Document downloadNonStructuredDocument(final RedisDocument redisDocument) {
+        final String documentAddress = DocumentUtils.readDocumentAddress(redisDocument.getDocumentID());
+        final String deployedHash = this.documentTransaction.getHash(documentAddress);
+
+        final byte[] buffer = downloadPDFDocumentFromS3(redisDocument.getDocumentID());
+        final String calculatedHash = DocumentUtils.getSha256(buffer);
+
+        if (!deployedHash.equals(calculatedHash)) {
+            log.info("[SERVICE] hash is not valid; expected: '{}', actual: '{}'", deployedHash, calculatedHash);
+            throw new DocumentNotFoundException("Document with '" + redisDocument.getDocumentID() + "' does not found");
+        }
+        final InputStream signedDoc = this.cronicaCAAdapter.signDocument(new ByteArrayInputStream(buffer));
+        final String fileName = "DC-" + redisDocument.getDocumentID() + ".pdf";
+
+        return Document.newInstance(fileName, signedDoc);
+    }
+
+    private byte[] downloadPDFDocumentFromS3(final String documentId) {
+        final String fileKey = "docs/DC-" + documentId + ".pdf";
+        if ( !this.repeater.apply(this.awss3BucketAdapter::fileExists, fileKey) ) {
+            throw new DocumentNotFoundException("Document with given " + documentId + " ID is not found");
+        }
+        return this.repeater.apply(this.awss3BucketAdapter::downloadFile, fileKey);
+    }
+
 }
--- src/main/java/io/cronica/api/pdfgenerator/utils/DocumentUtils.java
+++ src/main/java/io/cronica/api/pdfgenerator/utils/DocumentUtils.java
@@ -1,12 +1,31 @@
 package io.cronica.api.pdfgenerator.utils;
 
+import io.cronica.api.pdfgenerator.exception.InvalidRequestException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.web3j.crypto.Hash;
+import org.web3j.utils.Numeric;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class DocumentUtils {
 
-    public static Map<String, Object> modifyParameters(Map<String, Object> initialMap) {
+    public static String getSha256(final byte[] bytes) {
+        return Numeric.toHexStringNoPrefix(Hash.sha256(bytes));
+    }
+
+    public static String readDocumentAddress(final String documentID) {
+        if (StringUtils.isEmpty(documentID) || !documentID.substring(0, 2).equals("0x")) {
+            log.info("[UTILITY] sent string is not ID of document");
+            throw new InvalidRequestException("Sent string is not ID of document");
+        }
+        return StringUtils.substring(documentID, 0, 42);
+    }
+
+    public static Map<String, Object> modifyParameters(final Map<String, Object> initialMap) {
         final Map<String, Object> modifiedMap = new HashMap<>();
         for (String key : initialMap.keySet()) {
             // if parameter in JSON is mapped to collection - skip it
