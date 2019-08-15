008ede195d48415a3e2348ad83d6ad57a1f89b54
Dmytro Kohut <retr0finr1r@gmail.com>
2019-08-15 11:15:08 +0300

    LZ4 decompression integrated for templates.

--- src/main/java/io/cronica/api/pdfgenerator/service/TemplateTransactionServiceImpl.java
+++ src/main/java/io/cronica/api/pdfgenerator/service/TemplateTransactionServiceImpl.java
@@ -5,9 +5,11 @@ import static io.cronica.api.pdfgenerator.utils.Constants.GAS_PRICE;
 
 import io.cronica.api.pdfgenerator.component.wrapper.TemplateContract;
 import io.cronica.api.pdfgenerator.exception.QuorumTransactionException;
+import io.cronica.api.pdfgenerator.utils.LZ4Utils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.web3j.abi.datatypes.DynamicBytes;
 import org.web3j.abi.datatypes.Utf8String;
 import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.crypto.Credentials;
@@ -16,6 +18,7 @@ import org.web3j.quorum.Quorum;
 import org.web3j.quorum.tx.ClientTransactionManager;
 import org.web3j.tx.gas.StaticGasProvider;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 @Slf4j
@@ -62,16 +65,25 @@ public class TemplateTransactionServiceImpl implements TemplateTransactionServic
     }
 
     private String readMainContentFromSmartContract(final TemplateContract templateContract) throws Exception {
-        final int parts = getSizeOfTemplateMainContent(templateContract);
-        if (parts == 0) {
-            return "";
+        final DynamicBytes bytes = templateContract.mainContent().send();
+        String data;
+        if (bytes == null) {
+            final int parts = getSizeOfTemplateMainContent(templateContract);
+            if (parts == 0) {
+                return "";
+            }
+            final StringBuilder fullText = new StringBuilder();
+            for (int index = 0; index < parts; index++) {
+                final Utf8String string = templateContract.mainContentOld(new Uint256(index)).send();
+                fullText.append(string.getValue());
+            }
+            data = fullText.toString();
         }
-        final StringBuilder fullText = new StringBuilder();
-        for (int index = 0; index < parts; index++) {
-            final Utf8String string = templateContract.mainContent(new Uint256(index)).send();
-            fullText.append(string.getValue());
+        else {
+            final byte[] decompressedData = LZ4Utils.decompress(bytes.getValue());
+            data = new String(decompressedData, StandardCharsets.UTF_8);
         }
-        return fullText.toString();
+        return data;
     }
 
     private int getSizeOfTemplateMainContent(final TemplateContract templateContract) throws Exception {
@@ -100,16 +112,25 @@ public class TemplateTransactionServiceImpl implements TemplateTransactionServic
     }
 
     private String readHeaderContentFromSmartContract(final TemplateContract templateContract) throws Exception {
-        final int parts = getSizeOfTemplateHeaderContent(templateContract);
-        if (parts == 0) {
-            return "";
+        final DynamicBytes bytes = templateContract.headerContent().send();
+        String data;
+        if (bytes == null) {
+            final int parts = getSizeOfTemplateHeaderContent(templateContract);
+            if (parts == 0) {
+                return "";
+            }
+            final StringBuilder fullText = new StringBuilder();
+            for (int index = 0; index < parts; index++) {
+                final Utf8String string = templateContract.headerContentOld(new Uint256(index)).send();
+                fullText.append(string.getValue());
+            }
+            data = fullText.toString();
         }
-        final StringBuilder fullText = new StringBuilder();
-        for (int index = 0; index < parts; index++) {
-            final Utf8String string = templateContract.headerContent(new Uint256(index)).send();
-            fullText.append(string.getValue());
+        else {
+            final byte[] decompressedData = LZ4Utils.decompress(bytes.getValue());
+            data = new String(decompressedData, StandardCharsets.UTF_8);
         }
-        return fullText.toString();
+        return data;
     }
 
     private int getSizeOfTemplateHeaderContent(final TemplateContract templateContract) throws Exception {
@@ -138,16 +159,25 @@ public class TemplateTransactionServiceImpl implements TemplateTransactionServic
     }
 
     private String readFooterContentFromSmartContract(final TemplateContract templateContract) throws Exception {
-        final int parts = getSizeOfTemplateFooterContent(templateContract);
-        if (parts == 0) {
-            return "";
+        final DynamicBytes bytes = templateContract.footerContent().send();
+        String data;
+        if (bytes == null) {
+            final int parts = getSizeOfTemplateFooterContent(templateContract);
+            if (parts == 0) {
+                return "";
+            }
+            final StringBuilder fullText = new StringBuilder();
+            for (int index = 0; index < parts; index++) {
+                final Utf8String string = templateContract.footerContentOld(new Uint256(index)).send();
+                fullText.append(string.getValue());
+            }
+            data = fullText.toString();
         }
-        final StringBuilder fullText = new StringBuilder();
-        for (int index = 0; index < parts; index++) {
-            final Utf8String string = templateContract.footerContent(new Uint256(index)).send();
-            fullText.append(string.getValue());
+        else {
+            final byte[] decompressedData = LZ4Utils.decompress(bytes.getValue());
+            data = new String(decompressedData, StandardCharsets.UTF_8);
         }
-        return fullText.toString();
+        return data;
     }
 
     private int getSizeOfTemplateFooterContent(final TemplateContract templateContract) throws Exception {
