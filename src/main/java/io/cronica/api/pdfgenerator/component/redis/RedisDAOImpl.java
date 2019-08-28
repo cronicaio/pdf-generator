421ba5074db1c91b44ffc6c6431e38c93268626a
Dmytro Kohut <retr0finr1r@gmail.com>
2019-08-28 17:38:39 +0300

    Methods for saving and reading PDF document from Redis have been implemented.

--- src/main/java/io/cronica/api/pdfgenerator/component/redis/RedisDAOImpl.java
+++ src/main/java/io/cronica/api/pdfgenerator/component/redis/RedisDAOImpl.java
@@ -7,10 +7,14 @@ import org.redisson.api.RBinaryStream;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class RedisDAOImpl implements RedisDAO {
+
+    private static final long TIME_TO_LIVE_HOURS = 24;
 
     private final RedissonClient redissonClient;
 
@@ -21,7 +25,7 @@ public class RedisDAOImpl implements RedisDAO {
      */
     @Override
     public RedisDocument get(final String key) {
-        log.info("[REDIS] reading object with '{}' key", key);
+        log.info("[REDIS] reading object under '{}' key", key);
         final RBinaryStream rBinaryStream = this.redissonClient.getBinaryStream(key);
         log.info("[REDIS] found object under '{}' key", key);
         return this.kryoSerializer.deserialize(rBinaryStream.get());
@@ -35,5 +39,29 @@ public class RedisDAOImpl implements RedisDAO {
         log.debug("[REDIS] check whether exists object under '{}' key", key);
         final RBinaryStream rBinaryStream = this.redissonClient.getBinaryStream(key);
         return rBinaryStream.isExists() && rBinaryStream.size() != 0;
+    }
+
+    /**
+     * @see RedisDAO#savePDF(byte[], String)
+     */
+    @Override
+    public boolean savePDF(final byte[] document, final String key) {
+        log.info("[REDIS] saving PDF document under key '{}' ", key);
+        final RBinaryStream rBinaryStream = this.redissonClient.getBinaryStream(key);
+        rBinaryStream.set(document);
+        rBinaryStream.expire(TIME_TO_LIVE_HOURS, TimeUnit.HOURS);
+        log.info("[REDIS] PDF document has been saved under '{}' key", key);
+        return true;
+    }
+
+    /**
+     * @see RedisDAO#getPDFByID(String)
+     */
+    @Override
+    public byte[] getPDFByID(final String key) {
+        log.info("[REDIS] reading PDF document under '{}' key", key);
+        final RBinaryStream rBinaryStream = this.redissonClient.getBinaryStream(key);
+        log.info("[REDIS] found PDF document under '{}' key", key);
+        return rBinaryStream.get();
     }
 }
